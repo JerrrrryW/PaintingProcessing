3134d47f20c255832ebf9a13f107dec7899c1e15
jerrrrry <1007648542@qq.com>
2022-07-19 17:14:32 +0800

    basic layout & preview of painting uploaded

--- app/src/main/java/com/example/paintingprocessing/MainActivity.java
+++ app/src/main/java/com/example/paintingprocessing/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.paintingprocessing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import static android.view.View.GONE;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int REQUEST_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_upload).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQUEST_IMAGE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK) {
+            //获取图片路径
+            if (requestCode == REQUEST_IMAGE && resultCode == Activity.RESULT_OK && intent != null) {
+                Uri selectedImage = intent.getData();
+                String[] filePathColumns = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePathColumns, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePathColumns[0]);
+                String imagePath = c.getString(columnIndex);
+                showImage(imagePath);
+                c.close();
+            }
+        }
+    }
+    //加载图片
+    private void showImage(String imagePath){
+        Bitmap bm = BitmapFactory.decodeFile(imagePath);
+        ((ImageView)findViewById(R.id.iv_preview)).setImageBitmap(bm);
     }
 }
