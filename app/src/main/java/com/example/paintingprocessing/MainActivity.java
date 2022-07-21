b8f9eb82a66c994dfcd724b78b21b7b51ab25b50
jerrrrry <1007648542@qq.com>
2022-07-21 15:54:13 +0800

    Set the availability of moving or zooming in/out the preview image.

--- app/src/main/java/com/example/paintingprocessing/MainActivity.java
+++ app/src/main/java/com/example/paintingprocessing/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.ThumbnailUtils;
@@ -17,6 +19,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -28,12 +31,24 @@ import java.util.List;
 
 import static android.view.View.GONE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
     static final int REQUEST_IMAGE = 1;
     RecyclerView recyclerView;
     List<PreviewInfo> datas;
     Bitmap inputBM;
     GalleryAdapter galleryAdapter;
+    private ImageView mImagevieqw;
+
+    private int MODE;//当前状态
+    public static final int MODE_NONE = 0;//无操作
+    public static final int MODE_DRAG = 1;//单指操作
+    public static final int MODE_SCALE = 2;//双指操作
+
+    private Matrix startMatrix = new Matrix();//初始矩阵
+    private Matrix endMatrix = new Matrix();//变化后的矩阵
+    private PointF startPointF = new PointF();//初始坐标
+    private float distance;//初始距离
+    private float scaleMultiple;//缩放倍数
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +67,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         initGallery();
+
+        mImagevieqw=findViewById(R.id.iv_preview);
+        mImagevieqw.setOnTouchListener(this);
+        mImagevieqw.setScaleType(ImageView.ScaleType.MATRIX);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -73,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
     //加载图片
     private void showImage(String imagePath){
         inputBM = resizeImage(imagePath,800,400);
-        ((ImageView)findViewById(R.id.iv_preview)).setImageBitmap(inputBM);
+        mImagevieqw.setImageBitmap(inputBM);
         refreshDataSet();
         galleryAdapter.notifyDataSetChanged();
     }
@@ -132,5 +151,62 @@ public class MainActivity extends AppCompatActivity {
 
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeFile(path, options);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()&event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN://单指触碰
+
+                //起始矩阵先获取ImageView的当前状态
+                startMatrix.set(mImagevieqw.getImageMatrix());
+                //获取起始坐标
+                startPointF.set(event.getX(), event.getY());
+                //此时状态是单指操作
+                MODE = MODE_DRAG;
+
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN://双指触碰
+
+                //最后的状态传给起始状态
+                startMatrix.set(endMatrix);
+                //获取距离
+                distance = getDistance(event);
+                //状态改为双指操作
+                MODE = MODE_SCALE;
+
+                break;
+            case MotionEvent.ACTION_MOVE://滑动（单+双）
+                if (MODE == MODE_DRAG) {//单指滑动时
+                    //获取初始矩阵
+                    endMatrix.set(startMatrix);
+                    //向矩阵传入位移距离
+                    endMatrix.postTranslate(event.getX() - startPointF.x, event.getY() - startPointF.y);
+                } else if (MODE == MODE_SCALE) {//双指滑动时
+                    //计算缩放倍数
+                    scaleMultiple = getDistance(event)/distance;
+                    //获取初始矩阵
+                    endMatrix.set(startMatrix);
+                    //向矩阵传入缩放倍数
+                    endMatrix.postScale(scaleMultiple, scaleMultiple,startPointF.x,startPointF.y);
+                }
+                break;
+            case MotionEvent.ACTION_UP://单指离开
+            case MotionEvent.ACTION_POINTER_UP://双指离开
+                //手指离开后，重置状态
+                MODE = MODE_NONE;
+                break;
+        }
+        //事件结束后，把矩阵的变化同步到ImageView上
+        mImagevieqw.setImageMatrix(endMatrix);
+        return true;
+
+    }
+
+    //获取距离
+    private static float getDistance(MotionEvent event) {//获取两点间距离
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
     }
 }
